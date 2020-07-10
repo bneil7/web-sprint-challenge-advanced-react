2c85cb4adf2ff68e5e0b0267a9ecae4519995948
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-07-10 11:27:25 -0600

    tests pass and fail properly

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -17,8 +17,9 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, handleChanges] = customHook;
   const customHook = useForm(initialValue);
+  const [values, handleChanges] = customHook;
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,43 +30,68 @@ const CheckoutForm = (props) => {
     <>
       <form onSubmit={handleSubmit}>
         <h2>Checkout Form</h2>
-        <label>
+
+        <label htmlFor="firstName">
           First Name:
           <input
+            id="firstName"
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
           />
         </label>
-        <label>
+
+        <label htmlFor="lastName">
           Last Name:
           <input
+            id="lastName"
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
           />
         </label>
-        <label>
+
+        <label htmlFor="address">
           Address:
           <input
+            id="address"
             name="address"
             value={values.address}
             onChange={handleChanges}
           />
         </label>
-        <label>
+
+        <label htmlFor="city">
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input 
+            id="city"
+            name="city" 
+            value={values.city} 
+            onChange={handleChanges} />
         </label>
-        <label>
+
+        <label htmlFor="state">
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input 
+            id="state"
+            name="state" 
+            value={values.state} 
+            onChange={handleChanges} />
         </label>
-        <label>
+
+        <label htmlFor="zip">
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input 
+            id="zip"
+            name="zip" 
+            value={values.zip} 
+            onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+
+        <label htmlFor="checkoutBtn">
+            <button>Checkout!</button>
+        </label>
+
       </form>
 
       {showSuccessMessage && (
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,63 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+// Arrange //
+    render(<CheckoutForm />);
+// Act //
+    const header = screen.getByText(/checkout form/i);
+// Assert //
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+// type into all inputs
+//      1. query for all inputs
+//      2. run the change event to add text
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const addressInput = screen.getByLabelText(/address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+
+    // events with RTL!
+    fireEvent.change(firstNameInput, {target: {value: 'Wade'} });
+    fireEvent.change(lastNameInput, {target: {value: 'Boggs'} });
+    fireEvent.change(addressInput, {target: {value: '123 Main St'} });
+    fireEvent.change(cityInput, {target: {value: 'Freedomland'} });
+    fireEvent.change(stateInput, {target: {value: 'NY'} });
+    fireEvent.change(zipInput, {target: {value: 42069} });
+    
+
+// click checkout! button
+//      1. query for the button
+//      2. run the click event on the button
+    const checkoutButton = screen.getByText(/checkout!/i)
+    fireEvent.click(checkoutButton);
+
+// assert checkout 
+//      1. query 
+//      2. assert inputs rendering
+    const newFirst = screen.getByText(/wade/i);
+    expect(newFirst).toBeInTheDocument();
+
+    const newLast = screen.getByText(/boggs/i);
+    expect(newLast).toBeInTheDocument();
+
+    const newAddress = screen.getByText(/123 main st/i);
+    expect(newAddress).toBeInTheDocument();
+
+    const newCity = screen.getByText(/freedomland/i);
+    expect(newCity).toBeInTheDocument();
+
+    const newState = screen.getByText(/ny/i);
+    expect(newState).toBeInTheDocument();
+
+    const newZip = screen.getByText(/42069/i);
+    expect(newZip).toBeInTheDocument();
+});
